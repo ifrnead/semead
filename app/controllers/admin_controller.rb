f25f89772695a859a1199704795501facacc4c4b
Jalerson Lima <jalerson@gmail.com>
2017-04-13 17:50:57 -0300

    Implementando submissão avulsa para o administrador.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,7 @@ class AdminController < ApplicationController
   before_filter :auth_required
 
   def index
-    if current_user.tem_perfil?('membro_comissao_cientifica') or current_user.tem_perfil?('coordenador_linha_pesquisa') or current_user.tem_perfil?('coordenador_comissao_cientifica') or current_user.tem_perfil?('membro_comissao_central')
+    if current_user.tem_perfil?('membro_comissao_cientifica') or current_user.tem_perfil?('coordenador_linha_pesquisa') or current_user.tem_perfil?('coordenador_comissao_cientifica') or current_user.tem_perfil?('membro_comissao_central') or current_user.admin?
       @trabalhos_avaliacao_pendente = current_user.autenticavel.trabalhos_avaliacao_pendente
     end
     if current_user.admin? or current_user.tem_perfil?('secretario')
